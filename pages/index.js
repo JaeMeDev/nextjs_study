66d4775316e1a048dc876720217f85f2a27d07cc
JaeMeDev <jaeme0406@gmail.com>
2022-04-06 19:36:28 +0900

    feat: fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "185d39589dc8bcf4f246fbe2248877ab";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      console.log(results);
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
