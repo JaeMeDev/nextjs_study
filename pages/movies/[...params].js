5fd35f6783613aff651b5bc17d5ca8c1e9252cb2
JaeMeDev <jaeme0406@gmail.com>
2022-04-07 15:25:06 +0900

    feat: Catch All

---
+++ pages/movies/[...params].js
@@ -0,0 +1,18 @@
+export default function Detail({ params }) {
+  const [title, id] = params || [];
+
+  return (
+    <div>
+      <Seo title={title} />
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+export function getServerSideProps({ params: { params } }) {
+  return {
+    props: {
+      params,
+    },
+  };
+}
